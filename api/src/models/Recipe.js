d70b14341f6a41898ba3b9035d7fc806e6dd7c01
punklodawn <punklodawn@hotmail.com>
2021-06-08 02:19:16 -0300

    BD creada

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -2,11 +2,38 @@ const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('recipe', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+    // defino el modelo
+    sequelize.define('recipe', {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        image: {
+            type: DataTypes.STRING,
+        },
+
+        summary: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+
+        score: {
+            type: DataTypes.INTEGER,
+        },
+
+        healthScore: {
+            type: DataTypes.INTEGER,
+        },
+
+        instructions: {
+            type: DataTypes.TEXT,
+        },
+
+        mine: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false,
+        },
+
+    });
 };
